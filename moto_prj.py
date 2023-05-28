2d79c88c725d55fa7c60440b7e86baa39f55e72e
ryoneue <ryoneue@gmail.com>
2023-05-29 00:51:45 +0900

    PC版で動くように改造

--- moto_prj.py
+++ moto_prj.py
@@ -1,16 +1,31 @@
 from tiny_line import tiny_line
-import urequests as requests
-import network
-import utime
-import socket
-from machine import Pin
-import time
-from wifi import Wifi
-#from pio_timer import pio_timer
-from ntp_date import ntp_date
-from machine import RTC
-import json
 
+
+import socket
+# from machine import Pin
+import time
+
+#from pio_timer import pio_timer
+
+# from machine import RTC
+import json
+import sys
+
+if "MicroPython" in sys.version:
+    import urequests as requests
+    from wifi import Wifi
+    from ntp_date import ntp_date
+    date = ntp_date()
+else:
+    import requests as requests
+    from datetime import datetime as date
+
+    
+head = """
+HTTP/1.1 200 OK
+Content-Type: text/html
+
+"""
 def set_wifi_info(json_file="info.json"):
     #Wi-FiのSSIDとパスワードを読み込み
     with open(json_file) as f:
@@ -18,9 +33,16 @@ def set_wifi_info(json_file="info.json"):
     ssid = info["ssid"]
     password = info["password"]
     access_token = info["access_token"]
-    net = Wifi(ssid, password)
-    status = net.status
+    if "MicroPython" in sys.version:
+        net = Wifi(ssid, password)
+        status = net.status
+        print("status: ",status)
+    else:
+        net = False
+        status = False
+        
     return net, status, access_token, ssid, password
+
 
 # html読み込み
 html = ""
@@ -28,16 +50,24 @@ with open("./moto.html", encoding='utf-8') as f:
     for line in f:
         html = html + line
 
-net, status, access_token, _, _ = set_wifi_info(json_file="info.json")
+
 
 # Open socket
-addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+net, status, access_token, _, _ = set_wifi_info(json_file="info.json")
+if "MicroPython" in sys.version:
+    print("status:", type(status[0]))    
+    addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+else:
+    host = 'localhost'
+    port = 8001    
+    addr = (host, port)
+    status = ['localhost:8001/']
 s = socket.socket()
 s.bind(addr)
 s.listen(1)
 
 # 通知用Line設定
-if not access_token==False or not access_token=="False": 
+if "MicroPython" in sys.version and (not access_token==False or not access_token=="False"): 
     tl = tiny_line(access_token, debug=True)
     print('listening on', addr)
     tl.notify("http://"+status[0])
@@ -57,22 +87,26 @@ M3 = 30000
 
 count = 0
 value = 0
+
 # 以下ループ処理
 # 変数M1,M2,M3を更新したWebページを作成する（もっと頻度を落としてもいいかも）
 while True:
     try:
-        date = ntp_date()
+        now = date.now
+        # cl, addr = s.accept()
         cl, addr = s.accept()
         print('client connected from', addr)
-        request = cl.recv(1024)
+        request = cl.recv(1024).decode('utf-8')
         print("request:")
         print(request)
-        request = str(request)
+        # request = str(request)
         
         # %以下の変数がhtml内部に代入される
-        response = html % (date.date, M1, M2, M3)
-        cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
-        cl.send(response)
+        response = html % (date, M1, M2, M3)
+        if not "MicroPython" in sys.version:
+            response = head + response
+        cl.sendall(response.encode('utf-8'))        
+        # cl.send(response)
         cl.close()
         # 1秒ごとにページを作成しなおす
         time.sleep(1)

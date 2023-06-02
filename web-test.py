417216b47c4a03219755fe757745768602ff0bb8
ryoneue <ryoneue@gmail.com>
2023-06-03 01:43:47 +0900

    First init CI/CD.

---
+++ web-test.py
@@ -0,0 +1,27 @@
+import subprocess as sp
+import time
+import socket
+import sys
+from bs4 import BeautifulSoup
+import requests
+import psutil
+import requests
+from html_check import html_check
+
+# from requests.adapters import HTTPAdapter, Retry
+
+
+
+class TestCase:
+    def test_true(self):
+        cmd = "test.bat"
+        p = sp.Popen(cmd, stdout=sp.PIPE, shell=True, creationflags=sp.CREATE_NEW_PROCESS_GROUP)
+        time.sleep(3)
+        func = html_check()
+        M1,M2,M3 = func.check_text('http://127.0.0.1:8000')
+
+        parent = psutil.Process(p.pid)
+        for child in parent.children(recursive=True):
+            child.kill()
+        parent.kill()
+        assert [M1,M2,M3]==["10000","20000","30000"]

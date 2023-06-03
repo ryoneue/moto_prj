c40aa159ad22800d1278e5dab993c0098d636094
ryoneue <ryoneue@gmail.com>
2023-06-03 12:38:30 +0900

    Fix CI/CD test

--- web-test.py
+++ web-test.py
@@ -11,15 +11,22 @@ import json
 
 # from requests.adapters import HTTPAdapter, Retry
 
+def load_json(json_file="info.json"):
+    # 設定用jsonファイルの読み込み
+    with open(json_file) as f:
+        info = json.load(f)
+    return info
+
 class TestCase:
     def test_true(self):
         cmd = "test.bat"
+        info = load_json()
 
         # p = sp.Popen(cmd, stdout=sp.PIPE, shell=True, creationflags=sp.CREATE_NEW_PROCESS_GROUP)
         p = sp.Popen(cmd, stdout=sp.PIPE, shell=True)
         time.sleep(3)
         func = html_check()
-        M1,M2,M3 = func.check_text('http://127.0.0.1:80')
+        M1,M2,M3 = func.check_text('http://127.0.0.1:8000')
 
         parent = psutil.Process(p.pid)
         for child in parent.children(recursive=True):
@@ -39,4 +46,4 @@ class TestCase:
         for child in parent.children(recursive=True):
             child.kill()
         parent.kill()
-        assert [M1,M2,M3]==["1000","2000","3000"]
+        assert [M1,M2,M3]!=["1000","2000","3000"]

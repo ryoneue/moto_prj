0a65558399670682c5b88ce13ccc27e7b214f210
ryoneue <ryoneue@gmail.com>
2023-06-03 12:28:25 +0900

    Add false test

--- web-test.py
+++ web-test.py
@@ -7,13 +7,27 @@ import requests
 import psutil
 import requests
 from html_check import html_check
+import json
 
 # from requests.adapters import HTTPAdapter, Retry
 
-
-
 class TestCase:
     def test_true(self):
+        cmd = "test.bat"
+
+        # p = sp.Popen(cmd, stdout=sp.PIPE, shell=True, creationflags=sp.CREATE_NEW_PROCESS_GROUP)
+        p = sp.Popen(cmd, stdout=sp.PIPE, shell=True)
+        time.sleep(3)
+        func = html_check()
+        M1,M2,M3 = func.check_text('http://127.0.0.1:80')
+
+        parent = psutil.Process(p.pid)
+        for child in parent.children(recursive=True):
+            child.kill()
+        parent.kill()
+        assert [M1,M2,M3]==["10000","20000","30000"]
+
+    def test_false(self):
         cmd = "test.bat"
         # p = sp.Popen(cmd, stdout=sp.PIPE, shell=True, creationflags=sp.CREATE_NEW_PROCESS_GROUP)
         p = sp.Popen(cmd, stdout=sp.PIPE, shell=True)
@@ -25,4 +39,4 @@ class TestCase:
         for child in parent.children(recursive=True):
             child.kill()
         parent.kill()
-        assert [M1,M2,M3]==["10000","20000","30000"]
+        assert [M1,M2,M3]==["1000","2000","3000"]
